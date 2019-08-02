84f31c616f785e087f7ba84a2f1d82f9ed345989
synguyencd3 <52818031+synguyencd3@users.noreply.github.com>
2019-08-02 13:27:00 +0700

    ads

---
+++ Assets/adController.cs
@@ -0,0 +1,30 @@
+﻿
+using UnityEngine;
+using UnityEngine.Monetization;
+
+public class adController : MonoBehaviour
+{
+    private string store_id = "3237816";
+    private string bannder_id = "Banner";
+    public bool testMode = true;
+    void Start()
+    {
+        Monetization.Initialize(store_id, testMode);
+      
+    }
+
+    
+   void Update()
+    {
+        if  (Monetization.IsReady(bannder_id))
+        {
+            ShowAdPlacementContent ad = null;
+            ad = Monetization.GetPlacementContent(bannder_id) as ShowAdPlacementContent;
+            if (ad != null)
+            {
+                ad.Show();
+            }
+        }
+
+    }
+}
