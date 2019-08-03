41b9ae4aba6bc71c8e36fd72ee95e1c44f623d5d
synguyencd3 <52818031+synguyencd3@users.noreply.github.com>
2019-08-03 16:35:53 +0700

    UI fix

---
+++ Assets/highscore.cs
@@ -0,0 +1,18 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class highscore : MonoBehaviour
+{
+    private TextMeshProUGUI score;
+    void Start()
+    {
+        score = GetComponent<TextMeshProUGUI>();
+        score.text = PlayerPrefs.GetInt("number", 0).ToString();
+    }
+
+    
+    void Update()
+    {
+        score.text = PlayerPrefs.GetInt("number", 0).ToString();
+    }
+}
