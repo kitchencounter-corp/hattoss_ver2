1f5f9011424e14aea8214d258a745288c6c733d0
synguyencd3 <52818031+synguyencd3@users.noreply.github.com>
2019-08-07 11:51:21 +0700

    fix lỗi ko dính

--- Assets/script/toss.cs
+++ Assets/script/toss.cs
@@ -10,12 +10,13 @@ public class toss : MonoBehaviour
     private Vector2 endswipe;
     public float force = 5f;
     public float torque = 5f;
+    private float magnitude;
     public screenmanager manager;
     private bool collidecheck = true;
 
     void Start()
     {
-        Application.targetFrameRate = 60;
+       Application.targetFrameRate = 60;
         rb = GetComponent<Rigidbody>();
         hat = GetComponent<Transform>();
         Time.timeScale = 0f;
@@ -32,8 +33,13 @@ public class toss : MonoBehaviour
         if (Input.GetMouseButtonUp(0))
         {
             endswipe = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+            magnitude = (endswipe - startswipe).magnitude;
+            Debug.Log(magnitude);
+            if (magnitude > 0.01f)
+            {
 
-            swipe();
+                swipe();
+            }
         }
     }
     void swipe()
@@ -41,24 +47,26 @@ public class toss : MonoBehaviour
         Time.timeScale = 1f;
         Vector2 Swipe = endswipe - startswipe;
         rb.AddForce(Swipe * force, ForceMode.Impulse);
-        rb.AddTorque(0f, 0f, torque, ForceMode.Impulse);
+        rb.AddTorque(0f, 0f, 1f, ForceMode.Impulse);
+      //  Debug.Log(torque * magnitude * 100);
     }
     IEnumerator OnTriggerEnter(Collider other)
     {
-        Debug.Log("dính");
+      //  Debug.Log("dính");
         collidecheck = true;
         Debug.Log(other.gameObject.tag);
+        Debug.Log(collidecheck);
         yield return new WaitForSeconds(0.9f);
-        if (collidecheck && other.tag != "Untagged")
+        if (collidecheck)// && other.tag != "Untagged")
         {
-            if (other.tag == "+1") { score.Scr++; Debug.Log("+1"); }
-            if (other.tag == "+2") { score.Scr = score.Scr + 2; Debug.Log("+2"); }
-            if (other.tag == "+3") { score.Scr = score.Scr + 3; Debug.Log("+3"); }
+            if (other.tag == "+1") { score.Scr++; Debug.Log("cong1"); }
+            if (other.tag == "+2") { score.Scr = score.Scr + 2; Debug.Log("cong2"); }
+            if (other.tag == "+3") { score.Scr = score.Scr + 3; Debug.Log("cong3"); }
             Debug.Log(score.Scr.ToString());
             Time.timeScale = 0f;
-            this.gameObject.SetActive(true);
+            this.gameObject.SetActive(false);
+           this.gameObject.SetActive(true);
             respawn(hat);
-            collidecheck = false;
         }
 
     }
@@ -66,7 +74,7 @@ public class toss : MonoBehaviour
     {
         if (collide.gameObject.tag == "floor")
         {
-            Debug.Log("floor hit");
+          //  Debug.Log("floor hit");
             yield return new WaitForSeconds(1f);
           //  manager.scoreboard.SetActive(false);
             manager.Gameover.SetActive(true);
@@ -84,7 +92,7 @@ public class toss : MonoBehaviour
     }
     public static void respawn(Transform hat)
     {
-        hat.position = new Vector3(-1.5f, 3.97f, -4f);
+        hat.position = new Vector3(-1.5f, 3.3f, -4.29f);
         hat.eulerAngles = new Vector3(-120f, 90f, 0f);
     }
 }
